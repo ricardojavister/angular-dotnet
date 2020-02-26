dc734946ae111e62520040acbb8f63365061c78c
unknown <ricardojavister@gmail.com>
2020-02-26 15:53:10 -0300

    adding crud

--- apiapp/Controllers/productController.cs
+++ apiapp/Controllers/productController.cs
@@ -20,11 +20,33 @@ namespace apiapp.Controllers
             return _context.Product.AsQueryable();
         }
 
+        [HttpGet]   
+        [Route("getById")]
+        public IQueryable<product> getById(int idProduct){
+            return _context.Product.Where(x=> x.idproduct==idProduct).AsQueryable();
+        }
+
         [HttpPost]
         public IActionResult insert([FromBody] product product){
             _context.Product.Add(product);
             var result = _context.SaveChanges();
             return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult update([FromBody] product product){
+            _context.Product.Update(product);
+            product entityFound =  this.getById(product.idproduct).FirstOrDefault();
+            entityFound.productName = product.productName;
+            entityFound.price = product.price;
+            return Ok(_context.SaveChanges());
+        }
+
+        [HttpDelete]
+        public IActionResult delete(int idProduct){
+            product entityFound =  this.getById(idProduct).FirstOrDefault();
+            _context.Product.Remove(entityFound);
+            return Ok(_context.SaveChanges());
         }
    
     }
